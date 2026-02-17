efa7a240638be769839ce51304e803b41712981c
Kauhã Érick de Costa <kauha.costa@stara.com.br>
2026-02-17 11:43:10 -0300

    Commiting 1h of course.

---
+++ src/App.vue
@@ -0,0 +1,23 @@
+<script setup>
+import Layout from './components/layouts/Layout.vue';
+import Dashboard from './components/pages/Dashboard.vue';
+import Welcome from './components/pages/Welcome.vue';
+import Workout from './components/pages/Workout.vue';
+
+const selectedDisplay = 1;
+</script>
+
+<template>
+  <Layout>
+    <!-- PAGE 1 -->
+    <Welcome />
+    <!-- PAGE 2 -->
+    <Dashboard/>
+    <!-- PAGE 3 -->
+    <Workout/>
+  </Layout>
+</template>
+
+<style scoped>
+
+</style>
